e2795bb79c698f3b02ecfec8997c02d143ce380e
mghanmy <ghanmy.mohsen@gmail.com>
2019-06-27 11:07:05 +0100

    Test
    Signed-off-by: mghanmy <ghanmy.mohsen@gmail.com>

---
+++ tests/ClassTestUnitaire.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace App\Tests;
+
+
+
+
+
+use PHPUnit\Framework\TestCase;
+
+class ClassTestUnitaire extends TestCase
+{
+  function testDeleteCalls(){
+      $this->assertEquals(42, 42);
+      $this->assertEquals(11, 42);
+      $this->assertTrue();
+  }
+}
